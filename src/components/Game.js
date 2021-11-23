3d25e616e19cfa69e24b861be3234a400fc9a374
M2A1G <matteoguarnaccia@outlook.it>
2021-11-23 14:38:18 +0100

    7. Syling Game Cards

---
+++ src/components/Game.js
@@ -0,0 +1,28 @@
+import React from "react";
+//Styling and Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const Game = ({ name, released, image }) => {
+  return (
+    <StyledGame>
+      <h3>{name}</h3>
+      <p>{released}</p>
+      <img src={image} alt={name} />
+    </StyledGame>
+  );
+};
+
+const StyledGame = styled(motion.div)`
+  min-height: 40vh;
+  box-shadow: 0px 5px 20px rgba(68, 5, 5, 0.2);
+  text-align: center;
+  border-radius: 1rem;
+  img {
+    width: 100%;
+    height: 40vh;
+    object-fit: cover;
+  }
+`;
+
+export default Game;
