17e9f27157181a41c565bbffc1ea564b3fa55230
M2A1G <matteoguarnaccia@outlook.it>
2021-11-24 15:40:18 +0100

    16. Rendering Rating Stars

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -6,13 +6,16 @@ import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { smallImage } from "../util";
-//IMAGES
+//PLATFORM IMAGES
 import playstation from "../img/playstation.svg";
 import steam from "../img/steam.svg";
 import xbox from "../img/xbox.svg";
 import nintendo from "../img/nintendo.svg";
 import apple from "../img/apple.svg";
 import gamepad from "../img/gamepad.svg";
+//STAR IMAGES
+import starEmpty from "../img/star-empty.png";
+import starFull from "../img/star-full.png";
 
 const GameDetail = ({ pathId }) => {
   const navigate = useNavigate();
@@ -23,6 +26,19 @@ const GameDetail = ({ pathId }) => {
       document.body.style.overflow = "auto";
       navigate("/");
     }
+  };
+  //GET STARS
+  const getStars = () => {
+    const stars = [];
+    const rating = Math.floor(game.rating);
+    for (let i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        stars.push(<img alt="star" key={i} src={starFull} />);
+      } else {
+        stars.push(<img alt="star" key={i} src={starEmpty} />);
+      }
+    }
+    return stars;
   };
 
   //GET PLATFORM IMAGE
@@ -58,6 +74,7 @@ const GameDetail = ({ pathId }) => {
               <div className="rating">
                 <h3>{game.name}</h3>
                 <p>Rating: {game.rating}</p>
+                {getStars()}
               </div>
               <Info>
                 <h3>Platforms</h3>
@@ -133,6 +150,11 @@ const Stats = styled(motion.div)`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  img {
+    width: 2rem;
+    height: 2rem;
+    display: inline;
+  }
 `;
 
 const Info = styled(motion.div)`
